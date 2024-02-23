53cb12b67e3de4e01bc6259cc0f84059ad2497b6
Steven <tucorreo@example.com>
2024-02-23 11:15:36 -0500

    Funcion boton de descarga

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Nav from "./components/Nav";
+import Buttondownload from "./components/Buttondownload";
 import "./styles/nav.css";
-
 
 function App() {
   return (
-    <div className="nav-buttons">
-      <Nav Navbutton="Inicio" />
-      <Nav Navbutton="Proyectos" />
-      <Nav Navbutton="Contacto" />
-      <Nav Navbutton="Sobre mi" />
+    <div>
+      <div className="nav-buttons">
+        <Nav Navbutton="Inicio" />
+        <Nav Navbutton="Proyectos" />
+        <Nav Navbutton="Contacto" />
+        <Nav Navbutton="Sobre mi" />
+      </div>
+      <div>
+        <Buttondownload Descargar="Descargar CV" />
+      </div>
     </div>
   );
 }
--- src/components/Buttondownload.jsx
+++ src/components/Buttondownload.jsx
@@ -1,10 +1,23 @@
-function Buttondownload({Descargar}) {
-  retrun(
-    <div>
-        <button className="cv" type="button">{Descargar}</button>
-    </div>
-    );
-}
+function Buttondownload({ Descargar }) {
+  const descargarCV = () => {
+    // Ruta de tu hoja de vida
+    const rutaCV = "/CV.pdf";
+    // Crea un enlace temporal
+    const link = document.createElement("a");
+    link.href = rutaCV;
+    // Asigna el nombre de archivo para la descarga
+    link.download = "Curriculum ";
+    // Simula un clic en el enlace para iniciar la descarga
+    link.click();
+  };
 
+  return (
+    <div>
+      <button className="cv" type="button" onClick={descargarCV}>
+        {Descargar}
+      </button>
+    </div>
+  );
+}
 
 export default Buttondownload;
